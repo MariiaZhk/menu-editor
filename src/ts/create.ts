99204f3bf91670a07d2a6945dd53517505ade0af
Mariia <maria8zhuk@gmail.com>
2024-11-28 12:43:43 +0100

    loadAll, add to menu functionality

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -1,1 +1,22 @@
+import { Pizza, PizzaProps } from "./models/Pizza";
 
+const form = document.querySelector(".create") as HTMLFormElement;
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  const newPizza: PizzaProps = {
+    title: data.get("title") as string,
+    description: data.get("description") as string,
+    toppings: data.getAll("topping") as string[],
+    price: parseInt(data.get("price") as string),
+  };
+  const res = await Pizza.save(newPizza);
+  if (!res.ok) {
+    console.log(
+      "Something went wrong... not able to add this pizza to the menu"
+    );
+  }
+  if (res.ok) {
+    window.location.href = "/";
+  }
+});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,27 @@
+import { Pizza, PizzaProps } from "./models/Pizza";
 
+const rootElement = document.querySelector(".root")!;
+function createPizzaTemplate(pizza: PizzaProps): string {
+  return `
+    <div class='pizza'>
+    <h2>${pizza.title}</h2>
+    <p class='toppings'>${pizza.toppings.join(", ")}<p>
+    <p>${pizza.description}</p>
+    <span>$${pizza.price}</span>
+
+    </div>
+    `;
+}
+
+function renderTemplate(template: string[], parent: Element): void {
+  const templateElement = document.createElement("template");
+  for (const t of template) {
+    templateElement.innerHTML += t;
+  }
+  parent.append(templateElement.content);
+}
+document.addEventListener("DOMContentLoaded", async () => {
+  const pizzas = await Pizza.loadAll();
+  const pizzaTemplates = pizzas.map(createPizzaTemplate);
+  renderTemplate(pizzaTemplates, rootElement);
+});
